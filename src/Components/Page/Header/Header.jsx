15d83f73b8b8e1a08aad53bdea6abce2a5c1e7c2
imshubhampatel <shubhampatel2024@gmail.com>
2022-05-05 08:42:21 +0530

    feat : added header

--- src/Components/Page/Header/Header.jsx
+++ src/Components/Page/Header/Header.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.css";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import DateRangeIcon from "@mui/icons-material/DateRange";
+import AddIcon from "@mui/icons-material/Add";
+import VideocamOutlinedIcon from "@mui/icons-material/VideocamOutlined";
+import InfoIcon from "@mui/icons-material/Info";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { EditOutlined } from "@mui/icons-material";
+import { DeleteOutlineOutlined } from "@mui/icons-material";
+import { InfoOutlined } from "@mui/icons-material";
 
-export default function Header() {
+export default function Header({
+  createUniversityBtn,
+  updateUniversityBtn,
+  deleteUniversityBtn,
+}) {
   return (
-    <div>Header</div>
-  )
+    <>
+      <div className={styles.parentHeader}>
+        <div className={`${styles.headerContent} ${styles.content}`}>
+          <div className={`${styles.headerLeft} ${styles.left}`}>
+            <div className={`${styles.text} ${styles.heading_text}`}>
+              Dashboard
+            </div>
+          </div>
+          <div className={`${styles.headerRight} ${styles.right}`}>
+            <div
+              className={` buttons_icon ${styles.buttons} ${styles.header_button}`}
+            >
+              <Tooltip title="plus">
+                <IconButton>
+                  <AddIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="edit">
+                <IconButton>
+                  <EditOutlined />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="delete">
+                <IconButton>
+                  <DeleteOutlineOutlined />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="info">
+                <IconButton>
+                  <InfoOutlined />
+                </IconButton>
+              </Tooltip>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import Layout from "../../Components/Page/Layout/Layout";
 
 export default function Dashboard() {
-  return <Layout>Dashboard</Layout>;
+  return <Layout></Layout>;
 }
